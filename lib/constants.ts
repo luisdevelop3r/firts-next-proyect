d65335dff823fc9027b22018db5b245d9f7c4647
luisdevelop3r <luisvalientedeveloper@gmail.com>
2025-11-13 19:40:40 -0500

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,84 @@
+export interface Event {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+}
+
+export const events: Event[] = [
+  {
+    title: "React Conf 2025",
+    image: "/images/event1.png",
+    slug: "react-conf-2025",
+    location: "Las Vegas, NV",
+    date: "2025-05-15",
+    time: "09:00 AM",
+  },
+  {
+    title: "Next.js Conference",
+    image: "/images/event2.png",
+    slug: "nextjs-conference-2025",
+    location: "San Francisco, CA",
+    date: "2025-06-20",
+    time: "10:00 AM",
+  },
+  {
+    title: "DevWorld Hackathon",
+    image: "/images/event3.png",
+    slug: "devworld-hackathon-2025",
+    location: "Austin, TX",
+    date: "2025-07-10",
+    time: "08:00 AM",
+  },
+  {
+    title: "TypeScript Summit",
+    image: "/images/event4.png",
+    slug: "typescript-summit-2025",
+    location: "Seattle, WA",
+    date: "2025-08-05",
+    time: "09:30 AM",
+  },
+  {
+    title: "Web3 Developers Meetup",
+    image: "/images/event5.png",
+    slug: "web3-developers-meetup",
+    location: "New York, NY",
+    date: "2025-09-12",
+    time: "06:00 PM",
+  },
+  {
+    title: "AI & Machine Learning Expo",
+    image: "/images/event6.png",
+    slug: "ai-ml-expo-2025",
+    location: "Boston, MA",
+    date: "2025-10-18",
+    time: "10:00 AM",
+  },
+  {
+    title: "Full Stack Developers Conference",
+    image: "/images/event1.png",
+    slug: "fullstack-conf-2025",
+    location: "Chicago, IL",
+    date: "2025-11-08",
+    time: "09:00 AM",
+  },
+  {
+    title: "DevOps Days",
+    image: "/images/event2.png",
+    slug: "devops-days-2025",
+    location: "Portland, OR",
+    date: "2025-12-03",
+    time: "08:30 AM",
+  },
+  {
+    title: "JavaScript Global Summit",
+    image: "/images/event3.png",
+    slug: "js-global-summit-2025",
+    location: "Miami, FL",
+    date: "2026-01-15",
+    time: "10:00 AM",
+  },
+];
+
